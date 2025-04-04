37b794dc3a62d42606e9f8d88a859b7f1b29d730
Benjamin Wong <b62wong@co001526-gmac.nexus.uwaterloo.ca>
2025-04-04 15:53:16 -0400

    learning and stuff

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,14 @@ module Main where
 
 import qualified MyLib (someFunc)
 import LinAlg
+import Numeric.LinearAlgebra
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   MyLib.someFunc
   putStrLn . show . mySeq $ 5
+
+function :: Int -> Int
+function x = x^2 
+
--- src/LinAlg.hs
+++ src/LinAlg.hs
@@ -1,7 +1,17 @@
 module LinAlg where
 
+import Numeric.LinearAlgebra
+
 main :: IO ()
 main = putStrLn "hey there"
 
 mySeq :: a -> [a]
 mySeq a = [a]
+
+a :: Matrix C
+a = (3 ><3)
+ [ 1.0, 2.0, 3.0
+ , 2.0, 4.0, 5.0
+ , 3.0, 5.0, 6.0 ]
+
+(l, v) = eigSH . trustSym $ a
